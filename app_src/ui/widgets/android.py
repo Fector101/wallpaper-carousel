40085e0e367fdaa588c2fbb8e7c91c7eaadf8181
fabian <fabianjoseph063@gmail.com>
2026-01-17 00:59:19 +0100

    service file patch

--- app_src/ui/widgets/android.py
+++ app_src/ui/widgets/android.py
@@ -1,5 +1,7 @@
-try:
+from android_notify.config import on_android_platform, from_service_file
+
+if on_android_platform() and not from_service_file():
     from kivymd.toast import toast
-except TypeError:
+else:
     def toast(text=None,length_long=0):
         print(f'Fallback toast - text: {text}, length_long: {length_long}')
