c59701c73d63bc90d976c0b976a7141556943417
Fabian <fabianjoseph063@gmail.com>
2026-03-06 19:14:04 +0100

    better starting title

--- app_src/android/services/wallpaper.py
+++ app_src/android/services/wallpaper.py
@@ -4,8 +4,8 @@ from utils.helper import write_logs_to_file
 write_logs_to_file()
 
 from android_notify import Notification, logger as android_notify_logger
-from android_notify.config import get_python_service, on_android_platform, autoclass
-from android_notify.internal.java_classes import BuildVersion
+from android_notify.config import get_python_service, on_android_platform
+from android_notify.internal.java_classes import BuildVersion, autoclass
 
 from utils.logger import app_logger
 from utils.constants import SERVICE_PORT_ARGUMENT_KEY
@@ -20,7 +20,7 @@ service = get_python_service()
 foreground_type = autoclass("android.content.pm.ServiceInfo").FOREGROUND_SERVICE_TYPE_SPECIAL_USE if on_android_platform() and BuildVersion.SDK_INT >= 30 else 0
 
 Notification.createChannel(id="service_channel",name="Carousel Service",description="For Controlling and Previewing Next Wallpaper")
-notification = Notification(title="Next in 02:00", name="from service",channel_id="service_channel",id=101)
+notification = Notification(title="Starting Carousel...", name="from service",channel_id="service_channel",id=101)
 notification.setData({"next wallpaper path": "test.jpg", SERVICE_PORT_ARGUMENT_KEY: receivedData.service_port})
 notification.setObeyUserClear(True) # don't show after users clear from Tray
 notification.addButton(text="Stop", receiver_name="CarouselReceiver", action="ACTION_STOP")
