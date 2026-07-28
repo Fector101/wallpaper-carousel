6ce08f693de41a0135bc2a4c2b5a1758e610e13b
Fabian <fabianjoseph063@gmail.com>
2026-07-28 10:30:29 +0100

    fix: not consuming the key press if it's not the esc key

--- app_src/ui/widgets/layouts.py
+++ app_src/ui/widgets/layouts.py
@@ -361,7 +361,7 @@ class MyMDScreen(MDScreen):
 
     def _on_key_down(self, _, key, *__):
         if key != 27:
-            return True
+            return False
         now = time.monotonic()
         if not self._back_key_pending:
             self._back_key_pending = True
@@ -376,7 +376,7 @@ class MyMDScreen(MDScreen):
 
     def _on_key_up(self, _, key, *__):
         if key != 27:
-            return True
+            return False
         now = time.monotonic()
         if not self._back_key_pending:
             # app_logger.info(f"[BackKey] MyMDScreen UP screen='{self.name}' -> ignored (no pending key_down)")
